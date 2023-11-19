790cc793f7c9d499d03c7fc3ffffb8d8cd441789
netcam22 <annettelesage@btinternet.com>
2023-11-19 16:20:24 +0000

    fix: remove unneccessary test files

--- testsrc/index.ts
+++
@@ -1,6 +0,0 @@
-document.getElementById("demo")?.addEventListener("click", () => {
-  const myMum: HTMLElement | null = document.getElementById("mum");
-  if (myMum) {
-    myMum.innerHTML = "go and make the dinner";
-  }
-});
