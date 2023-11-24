0a449a23334ff4d3001b3c2d694d175cc86954d0
netcam22 <annettelesage@btinternet.com>
2023-11-24 09:46:58 +0000

    refactor: separate out draggable functions

--- src/ui/draggable.ts
+++ src/ui/draggable.ts
@@ -5,68 +5,83 @@ export function makeDraggable(thisRobotId: string) {
 
   if (thisRobot) {
     thisRobot.onmousedown = function (event) {
-      const shiftX = event.clientX - thisRobot.getBoundingClientRect().left;
-      const shiftY = event.clientY - thisRobot.getBoundingClientRect().top;
-
-      document.body.append(thisRobot);
-      moveAt(event.pageX, event.pageY);
-
-      function moveAt(pageX: number, pageY: number) {
-        if (thisRobot) {
-          thisRobot.style.left = pageX - shiftX + "px";
-          thisRobot.style.top = pageY - shiftY + "px";
-        }
-      }
-
-      function onMouseMove(event: any) {
-        moveAt(event.pageX, event.pageY);
-      }
-
-      document.addEventListener("mousemove", onMouseMove);
-
-      function onMouseUp(event: any) {
-        if (thisRobot) {
-          thisRobot.hidden = true;
-          const elemBelow: Element | null = document.elementFromPoint(
-            event.clientX,
-            event.clientY
-          );
-          thisRobot.hidden = false;
-          if (elemBelow) {
-            const droppableBelow: Element | null = elemBelow.classList.contains(
-              "grid-layer"
-            )
-              ? elemBelow
-              : null;
-            if (droppableBelow) {
-              if (thisRobot.parentNode === document.body) {
-                document.body.removeChild(thisRobot);
-              }
-              thisRobot.style.left = "0%";
-              thisRobot.style.top = "-20%";
-              droppableBelow.append(thisRobot);
-
-              if (thisRobot.parentNode === droppableBelow) {
-                document.removeEventListener("mouseup", onMouseUp);
-                thisRobot.onmousedown = null;
-                event.preventDefault();
-                showMoveButtons();
-              }
-            }
-          }
-        }
-      }
-
-      document.addEventListener("mouseup", onMouseUp);
-
-      thisRobot.onmouseup = function () {
-        document.removeEventListener("mousemove", onMouseMove);
-        thisRobot.onmousemove = null;
-      };
-
-      thisRobot.ondragstart = function () {
-        return false;
-      };
+      startMove(event, thisRobot);
     };
   }
+
+  function startMove(event: MouseEvent, thisRobot: HTMLElement) {
+    const shiftX = event.clientX - thisRobot.getBoundingClientRect().left;
+    const shiftY = event.clientY - thisRobot.getBoundingClientRect().top;
+
+    document.body.append(thisRobot);
+
+    function dragThisRobot(event: any) {
+      shiftRobot(thisRobot, event.pageX, event.pageY, shiftX, shiftY);
+    }
+
+    document.addEventListener("mousemove", dragThisRobot);
+
+    document.addEventListener("mouseup", dropThisRobot);
+
+    thisRobot.onmouseup = function () {
+      document.removeEventListener("mousemove", dragThisRobot);
+      thisRobot.onmousemove = null;
+    };
+
+    thisRobot.ondragstart = function () {
+      return false;
+    };
+  }
+
+  function dropThisRobot(event: any) {
+    if (thisRobot) {
+      thisRobot.hidden = true;
+      const elemBelow: Element | null = document.elementFromPoint(
+        event.clientX,
+        event.clientY
+      );
+      thisRobot.hidden = false;
+      if (elemBelow) {
+        const dropZone = attachRobot(elemBelow, thisRobot);
+        if (dropZone && thisRobot.parentNode === dropZone) {
+          document.removeEventListener("mouseup", dropThisRobot);
+          thisRobot.onmousedown = null;
+          event.preventDefault();
+          showMoveButtons();
+        }
+      }
+    }
+  }
+}
+
+function shiftRobot(
+  thisRobot: HTMLElement,
+  pageX: number,
+  pageY: number,
+  shiftX: number,
+  shiftY: number
+) {
+  if (thisRobot) {
+    thisRobot.style.left = pageX - shiftX + "px";
+    thisRobot.style.top = pageY - shiftY + "px";
+  }
+}
+
+function attachRobot(
+  elemBelow: Element,
+  thisRobot: HTMLElement
+): Element | undefined {
+  const dropZone: Element | null = elemBelow.classList.contains("grid-layer")
+    ? elemBelow
+    : null;
+  if (dropZone) {
+    if (thisRobot.parentNode === document.body) {
+      document.body.removeChild(thisRobot);
+    }
+    thisRobot.style.left = "0%";
+    thisRobot.style.top = "-20%";
+    dropZone.append(thisRobot);
+    return dropZone;
+  }
+  return undefined;
 }
